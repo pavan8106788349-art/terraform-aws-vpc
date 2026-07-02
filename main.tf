cbe11e7ea6d051be6bd6b68bddbfa91ed11c6a4f
Sridharla pavan kumar <Pavan8106788349@gmail.com>
2026-07-02 14:18:18 +0530

    files

--- main.tf
+++ main.tf
@@ -6,3 +6,8 @@ resource "aws_vpc" "main" {
   tags = local.vpc_final_tags
 }
 
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id # VPC association
+
+  tags = local.igw_final_tags
+}
