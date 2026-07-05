12fbf0421097a48e4916b3de27fa8518d5261fb1
Sridharla pavan kumar <Pavan8106788349@gmail.com>
2026-07-05 11:28:31 +0530

    terraform

--- peering.tf
+++ peering.tf
@@ -47,9 +47,9 @@ resource "aws_route" "database_peering" {
   vpc_peering_connection_id = aws_vpc_peering_connection.default[count.index].id
 }
 
-resource "aws_route" "default_peering" {
+ resource "aws_route" "default_peering" {
   count = var.is_peering_required ? 1 : 0
   route_table_id            = data.aws_route_table.default.id
   destination_cidr_block    = var.vpc_cidr
-  vpc_peering_connection_id = aws_vpc_peering_connection.default[count.index].id
+   vpc_peering_connection_id = aws_vpc_peering_connection.default[count.index].id
 }
