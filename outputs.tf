d422e0777151068920cdad3b2e03fdff91024f7e
Sridharla pavan kumar <Pavan8106788349@gmail.com>
2026-08-19 12:29:35 +0530

    changed files

--- outputs.tf
+++ outputs.tf
@@ -17,3 +17,7 @@ output "private_subnet_ids" {
 output "database_subnet_ids" {
     value = aws_subnet.database[*].id
 }
+
+output "database_subnet_group_name" {
+    value = aws_db_subnet_group.roboshop.name
+}
